90ce92aee970e5c1ab570c3a7143df0221f3ac46
Unknown <jmubagosotomayor@gmail.com>
2018-11-05 22:07:14 +0100

    component lists posts and posts comments

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
+
+import * as _ from "lodash";
+
 import { DataService } from '../data.service';
-import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-posts',
@@ -9,13 +12,26 @@ import { Observable } from 'rxjs';
 })
 export class PostsComponent implements OnInit {
   posts$: Object;
+  postId: number;
+  postComments: Object;
   
   constructor(private data: DataService) { }
 
   ngOnInit() {
     this.data.getPosts().subscribe(
-      data => this.posts$ = data 
+      data => this.posts$ = data
     );
   }
+  
+  public postDetailId(p){
+    this.postId = p;
+    this.fetchPostData();
+  }
 
+  private fetchPostData(){
+    this.data.getComment(this.postId).subscribe(
+      data => {this.postComments = data;
+      }
+    );
+  }
 }
